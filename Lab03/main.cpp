e3e430f9bc48157bd0b336bf927478c6ae2e39e7
Kerise <sk44485@zut.edu.pl>
2020-04-08 22:46:38 +0200

    update

--- Lab03/main.cpp
+++ Lab03/main.cpp
@@ -1,14 +1,16 @@
 #include<iostream>
 #include<math.h>
+#include<fstream>
+#include<vector>
 using namespace std;
 #define PI 3.14159265
 class DFT_Coeff {
-   public:
-   double real, img;
-   DFT_Coeff() {
-      real = 0.0;
-      img = 0.0;
-   }
+public:
+    double real, img;
+    DFT_Coeff() {{
+        real= 0.0;
+        img= 0.0;
+    }}
 };
 double sygnal(double a,double f,double ro,double t)
 {
@@ -16,34 +18,52 @@ double sygnal(double a,double f,double ro,double t)
     double probka=a*sin(w*t+ro);
     return probka;
 }
+vector<DFT_Coeff> dft(int N,vector<double>x){
+    DFT_Coeff dft_value;
+    vector<DFT_Coeff>complex(N);
+    for(int j=0;j<N;j++){
+        complex[j]=dft_value;
+        for (int i = 0; i < N; i++) {
+            complex[j].real += x[i] * (cos(((-1)*2 * PI * j * i) / N));
+            complex[j].img += x[i] * (sin(((-1)*2 * PI * j * i) / N));
+        }}
+    return complex;
+}
 int main() {
-   int N = 854;
-   double a, b, c;
-   double x[N];
-   a=1.0;
-   double f=8;
-   double ro=4*M_PI;
-   int i=0;
-   for(double t=0;t<8.54;t=t+0.01){
-   x[i]=sygnal(a,f,ro,t);
-   i++;
-   }
-   double funkcja[N];
-   for (int i = 0; i < N; i++) {
-      funkcja[i] = (((a * (double) i) + (b * (double) i)) - c);
-   }
-   int k=i-1;
-   double cosin[N];
-   double sinu[N];
-   for (int n = 0; n < N; n++) {
-      cosin[n] = cos((2 * PI * k * n) / N);
-      sinu[n] = sin((2 * PI * k * n) / N);
-   }
-   DFT_Coeff dft_value;
-   cout << "The coeffs are: ";
-   for (int i = 0; i < N; i++) {
-      dft_value.real += x[i] * cosin[i];
-      dft_value.img += x[i] * sinu[i];
-   }
-   cout << "(" << dft_value.real << ") - " << "(" << dft_value.img << " i)";
+    int N = 20;
+    double a;
+    vector<double>x;
+    a = 1.0;
+    double f = 5;
+    double ro = 4 * M_PI;
+    int i = 0;
+    ofstream plik1;
+    double probka;
+    for (double t = 0; t < 1; t = t + ((double) 1 / (double) 20)) {
+        probka=sygnal(a, f, ro, t);
+        x.push_back(probka);
+        i++;
+    }
+    ifstream myFile;
+    myFile.open("f8.csv");
+    string p;
+    string q;
+    vector<double> data;
+    while (getline(myFile, p, ',')) {
+        getline(myFile, q);
+        double fun = stod(p);
+        data.push_back(fun);
+    }
+
+    vector<DFT_Coeff> complex(N);
+    complex = dft(N,data);
+    plik1.open("f1.csv");
+    double M[N];
+    for (size_t j = 0; j < N; j++) {
+        M[j] = sqrt(pow(complex[j].real, 2) + pow(complex[j].img, 2));
+        double decybel;
+        decybel = 10 * log10(M[j]);
+        plik1 << decybel<< "," << j << endl;
+    }
+    plik1.close();
 }
