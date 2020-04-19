cb34d9df0991ffcbb330f0ae850d54a83a56d9be
Kerise <sk44485@zut.edu.pl>
2020-04-19 23:13:16 +0200

    modulacje

--- Lab04/main.cpp
+++ Lab04/main.cpp
@@ -4,22 +4,24 @@
 #include<vector>
 #define PI 3.14159265
 using namespace std;
-vector<double> modulacja_amp(vector<double>x)
+vector<double> modulacja_amp(vector<double>y,vector<double>x)
 {
+    double kA=90;
     vector<double>mod;
     for(int j=0;j<x.size();j++)
     {
-        mod.push_back((90*x[j]+1)*cos(2*PI*1000*j));
+        mod.push_back((kA*y[j]+1)*cos(2*PI*1000*x[j]));
     }
     return mod;
 
 }
-vector<double> modulacja_faz(vector<double>x)
+vector<double> modulacja_faz(vector<double>y,vector<double>x)
 {
+    double kp=60;
     vector<double>mod;
     for(int j=0;j<x.size();j++)
     {
-        mod.push_back(cos(2*PI*1000*j+60*x[j]));
+        mod.push_back(cos(2*PI*1000*x[j]+kp*y[j]));
     }
     return mod;
 }
@@ -32,7 +34,7 @@ int main() {
     vector<double> y;
     vector<double> x;
     int i=1;
-    while (i<=501)
+    while (i<=22050)
     {
         getline(myFile, p,',');
         double fun = stod(p);
@@ -46,7 +48,7 @@ int main() {
     }
     myFile.close();
     vector<double>mod_amp;
-    mod_amp=modulacja_amp(y);
+    mod_amp=modulacja_amp(y,x);
     ofstream plik1;
     plik1.open("mod_a.csv");
     for(int i=0;i<mod_amp.size();i++)
@@ -55,7 +57,7 @@ int main() {
     }
     plik1.close();
     vector<double>mod_faz;
-    mod_faz=modulacja_faz(y);
+    mod_faz=modulacja_faz(y,x);
     plik1.open("mod_f.csv");
     for(int i=0;i<mod_faz.size();i++)
     {
