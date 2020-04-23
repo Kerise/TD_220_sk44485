09d41f174bd0af1dfe59d8a41b87dedb80589f9f
Kerise <sk44485@zut.edu.pl>
2020-04-23 14:48:40 +0200

    lab05

--- Lab05/main.cpp
+++ Lab05/main.cpp
@@ -46,28 +46,78 @@ string S2BS(string in,int switcher)
 vector<double> ASK(string binary)
 {
     double A1=1;
-    double A2=6;
-    double f=4;
+    double A2=8;
+    double f=100;
+    int wydluzenie=10;
     vector<double>out;
     string::iterator it;
     int index=1;
-    for(it=binary.begin();it<binary.end();it++,index+=10)
+    for(it=binary.begin();it<binary.end();it++)
     {
         if(*it=='0')
         {
-            out.push_back(A1*sin(2*PI*f*index+0));
+            for(int i=0;i<wydluzenie;i++) {
+                out.push_back(A1 * sin(2 * PI * f * index+0));
+            }
         }
         else if(*it=='1')
         {
-            out.push_back(A2*sin(2*PI*f*index+PI));
+            for(int i=0;i<wydluzenie;i++) {
+                out.push_back(A2 * sin(2 * PI * f* index+0));
+            }
         }
     }
     return out;
 
 }
-int main() {
+vector<double> FSK(string binary,double f1,double f2)
+{
+    double A=1;
+    int wydluzenie=10;
+    vector<double>out;
+    string::iterator it;
+    int index=1;
+    for(it=binary.begin();it<binary.end();it++)
+    {
+        if(*it=='0')
+        {
+            for(int i=0;i<wydluzenie;i++) {
+                out.push_back(A * sin(2 * PI * f1 * index+PI));
+            }
+        }
+        else if(*it=='1')
+        {
+            for(int i=0;i<wydluzenie;i++) {
+                out.push_back(A * sin(2 * PI * f2* index+PI));
+            }
+        }
+    }
+    return out;
+
+}
+vector<double>PSK(string binary) {
+    double A = 1;
+    double f=100;
+    int wydluzenie = 10;
+    vector<double> out;
+    string::iterator it;
+    int index = 1;
+    for (it = binary.begin(); it < binary.end(); it++) {
+        if (*it == '0') {
+            for (int i = 0; i < wydluzenie; i++) {
+                out.push_back(A * sin(2 * PI * f * index + 0));
+            }
+        } else if (*it == '1') {
+            for (int i = 0; i < wydluzenie; i++) {
+                out.push_back(A * sin(2 * PI * f * index + PI));
+            }
+        }
+    }
+    return out;
+}
+    int main() {
     int N=100;
-    double Tb=1;
+    double Tb=5;
     double f1,f2;
     f1=(N+1)/Tb;
     f2=(N+2)/Tb;
@@ -80,9 +130,26 @@ int main() {
     ask=ASK(binary);
     ofstream plik1;
     plik1.open("ask.csv");
-    for(int i=0;i<ask.size();i++)
+    cout<<ask.size();
+    for(double i=0;i<ask.size();i++)
     {
         plik1<<ask[i]<<","<<i<<endl;
+    }
+    plik1.close();
+    vector<double>fsk;
+    fsk=FSK(binary,f1,f2);
+    plik1.open("fsk.csv");
+    for(double i=0;i<fsk.size();i++)
+    {
+        plik1<<fsk[i]<<","<<i<<endl;
+    }
+    plik1.close();
+    vector<double>psk;
+    psk=PSK(binary);
+    plik1.open("psk.csv");
+    for(double i=0;i<psk.size();i++)
+    {
+        plik1<<psk[i]<<","<<i<<endl;
     }
     plik1.close();
     return 0;
